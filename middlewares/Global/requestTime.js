31b21c593892e6af2556379e2a1df775bda53b6d
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-16 14:54:04 +0200

    add log file

--- middlewares/Global/requestTime.js
+++ middlewares/Global/requestTime.js
@@ -1,3 +1,5 @@
+const fs = require("node:fs");
+
 const requestTime = (req, res, next) => {
   const now = new Date();
 
@@ -6,6 +8,16 @@ const requestTime = (req, res, next) => {
     now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
   const second =
     now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+  const currentTime = `${hours}:${minutes}:${second}`;
+
+  const content = `\n Richiesta ${req.method} sull'url ${req.path} alle ${hours}:${minutes}:${second}`;
+
+  fs.appendFile("./log/requestLog.txt", content, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 
   console.log(
     `Richiesta ${req.method} sull'url ${req.path} alle ${hours}:${minutes}:${second}`
